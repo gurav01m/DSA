a07d09f2da8384523daf69940b0ebe150d73b961
gurav01m <gauravphotosall@gmail.com>
2026-07-19 01:57:48 +0530

    Time: 3 ms (33.62%) | Memory: 12.4 MB (6.27%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string ans;
+        for(char c:s){
+            ans.push_back(c);
+            if(ans.size()>=part.size() && ans.substr(ans.size()-part.size())==part){
+                ans.erase(ans.size()-part.size());
+            }
+        }
+        return ans;
+    }
+};
