c6435f74b35842b97306b3fe367444eb6cca195f
gurav01m <gauravphotosall@gmail.com>
2026-07-27 08:32:05 +0530

    Time: 3 ms (46.58%) | Memory: 10.2 MB (26.16%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string s="a";
+        while(s.size()<k){
+            string t="";
+            for(char c:s){
+                t+= (c == 'z') ? 'a' : char(c + 1);
+            }
+            s+=t;
+        }
+        return s[k-1];
+    }
+};
