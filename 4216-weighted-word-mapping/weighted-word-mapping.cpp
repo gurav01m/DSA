c2b8b29ce3aae99d5b78e4b78ca97da6c23b26eb
gurav01m <gauravphotosall@gmail.com>
2026-07-30 23:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (97.38%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (string &word : words) {
+            int sum = 0;
+            for (char c : word) {
+                sum = (sum + weights[c - 'a']) % 26;
+            }
+            ans += char('a' + (25 - sum));
+        }
+        return ans;
+    }
+};
