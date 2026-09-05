955dbaefb8882e29b72379111512dbcc987aaefd
gurav01m <gauravphotosall@gmail.com>
2026-09-05 13:47:19 +0530

    Time: 8 ms (70.04%) | Memory: 202.7 MB (68.33%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> suffMin(n);
+        suffMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffMin[i] = min(nums[i], suffMin[i + 1]);
+        }
+        int maxi = nums[0];
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+            if (maxi - suffMin[i] <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
