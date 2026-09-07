16256f2f83725dead68e33e1ecfee08f7adcc07c
gurav01m <gauravphotosall@gmail.com>
2026-09-07 19:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (15.74%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const int MOD = 1e9 + 7;
+
+        vector<long long> dp(s.size() + 1, 0);
+        vector<int> last(26, 0);
+
+        // dp[0] = 1 represents the empty subsequence
+        dp[0] = 1;
+
+        for (int i = 1; i <= s.size(); i++) {
+            int c = s[i - 1] - 'a';
+
+            dp[i] = (2 * dp[i - 1]) % MOD;
+
+            if (last[c] > 0) {
+                dp[i] = (dp[i] - dp[last[c] - 1] + MOD) % MOD;
+            }
+
+            last[c] = i;
+        }
+
+        // Remove the empty subsequence
+        return (dp[s.size()] - 1 + MOD) % MOD;
+    }
+};
