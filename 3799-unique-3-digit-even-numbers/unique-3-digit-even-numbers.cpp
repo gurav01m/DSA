d00906ba041871f7dd4d1fa7edb31134c50fa77b
gurav01m <gauravphotosall@gmail.com>
2026-09-11 08:06:18 +0530

    Time: 75 ms (11.61%) | Memory: 61.8 MB (6.76%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        vector<int>curr(10,0);
-        for(int n:digits){
-            curr[n]++;
+        vector<int> freq(10, 0);
+
+
+        for (int d : digits) {
+            freq[d]++;
         }
-        vector<int>ans;
-        int res=0;
-        for(int i=100;i<=998;i+=2){
-            int a=i%10;
-            int b=(i/10)%10;
-            int c=i/100;
-            vector<int>need(10,0);
+
+        int ans = 0;
+
+        for (int num = 100; num <= 999; num++) {
+            if (num % 2 != 0)
+                continue;
+
+            int x = num;
+
+            int a = x % 10;  // units
+            x /= 10;
+
+            int b = x % 10;  // tens
+            x /= 10;
+
+            int c = x;       // hundreds
+
+           
+            vector<int> need(10, 0);
             need[a]++;
             need[b]++;
             need[c]++;
-            bool ok=true;
-            for(int i=0;i<10;i++){
-                if(need[i]>curr[i]){
-                    ok=false;
+
+            bool possible = true;
+
+            for (int d = 0; d <= 9; d++) {
+                if (need[d] > freq[d]) {
+                    possible = false;
                     break;
                 }
             }
-            if(ok)res++;
 
+            if (possible)
+                ans++;
         }
-        
-        return res;
 
+        return ans;
     }
 };
